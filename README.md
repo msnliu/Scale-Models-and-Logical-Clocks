6cd64b7e4c55cd4a12a68be034c84915a459d4c6
wan19990901 <wanguangya.com>
2023-03-06 19:11:32 -0500

    update readme and logical clock

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# Lamport Logical Clock
+# Introduction
 
-This is an implementation of the Lamport Logical Clock algorithm for distributed systems in Python. The code simulates a group of virtual machines that communicate with each other over sockets and use the Lamport algorithm to keep track of their logical clocks.
+This code implements a simple version of the Lamport clock algorithm using Python's multiprocessing and threading libraries. The Lamport clock is a logical clock algorithm that is used to order events in a distributed system. This code simulates a distributed system consisting of three virtual machines that communicate with each other over a network. Each virtual machine maintains a local logical clock, which is used to order the events that occur on that machine. The code records the messages sent and received by each machine, as well as the local clock values for each machine at different times.
 
 # Requirements
-Python 3.7 or higher
 
-multiprocessing, socket module
+Python 3.x
+multiprocessing library
+threading library
+
 
 # How to Run
 
@@ -21,6 +23,23 @@ The code will start three virtual machines on ports 2050, 3050, and 4050. Each v
 Each virtual machine is implemented as a separate process and runs on its own port. When a virtual machine receives a message from another virtual machine, it updates its logical clock based on the time stamp in the message. If a virtual machine has no messages to process, it generates an internal event and updates its logical clock accordingly.
 
 The Lamport logical clock algorithm is implemented in the LamportClock class. Each virtual machine is represented by the VirtualMachine class. The init_machine function sets up the server-side logic for each virtual machine. The machine function initializes each virtual machine and starts the server-side and client-side logic.
+
+# Output format
+
+The code records the messages sent and received by each virtual machine, as well as the local clock values for each machine at different times. The output is saved in separate log files for each machine (vm_0.log, vm_1.log, and vm_2.log).
+
+The log files contain the following information:
+
+Messages sent and received by the machine, along with the global time, the logical clock value of the sender, and the logical clock value of the receiver at the time the message was sent or received.
+Internal events that occur on the machine, along with the global time and the logical clock value of the machine at the time the event occurred.
+
+# Limitation 
+
+This code implements a simple version of the Lamport clock algorithm and has a number of limitations. Some of the limitations are:
+
+The code assumes that the virtual machines are running on the same physical machine and communicate with each other over a network. In a real distributed system, the machines would be running on separate physical machines and communicate with each other over a network.
+The code does not implement any fault tolerance or error handling mechanisms. In a real distributed system, fault tolerance and error handling would be essential.
+The code does not implement any optimizations to reduce the number of messages exchanged between the machines. In a real distributed system, optimizing the message exchange is critical to performance.
 
 # License
 This code is licensed under the MIT License. See the LICENSE file for details.
--- logical_clock.py
+++ logical_clock.py
@@ -4,23 +4,34 @@ import socket
 import threading
 import time
 import random
+
+
 class LamportClock:
     def __init__(self, tick_rate):
         self.time = 0
         self.lock = threading.Lock()
         self.tick_rate = tick_rate
+    
     def increment(self):
+        """Increment the clock by 1."""
         with self.lock:
             self.time += 1
+    
     def update(self, other_time):
+        """Update the clock with the maximum value of other_time and the current time."""
         with self.lock:
             self.time = max(self.time, other_time) + 1
+    
     def get_time(self):
+        """Get the current time on the clock."""
         with self.lock:
             return self.time
+    
     def wait(self):
-        # Wait for a certain amount of time based on the tick rate
+        """Wait for a certain amount of time based on the tick rate."""
         time.sleep(1/self.tick_rate)
+
+
 class VirtualMachine:
     def __init__(self, config, idx):
         self.host = config[0]
@@ -29,7 +40,9 @@ class VirtualMachine:
         self.clock = LamportClock(random.randint(1, 6))
         self.queue = []
         self.log_file = open(f"vm_{self.id}.log", "w")
+    
     def receive_messages(self, conn):
+        """Receive messages on a connection and add them to the message queue."""
         # Log the connection
         print(f"Connected to {conn.getpeername()}\n")
         while True:
@@ -40,14 +53,18 @@ class VirtualMachine:
             data_val, time_val = data.decode('ascii').split(",")
             # Add the received message to the local message queue
             self.queue.append((data_val, int(time_val)))
+    
     def connect(self,ports):
+        """Connect to the other machines on the specified ports."""
         sockets = []
         for p in ports:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((self.host, p))
             sockets.append(s) 
         return sockets
+    
     def send_message(self, conn):
+        """Send a message on the specified connection and update the clock."""
         # Get the current time from the logical clock
         time_val = self.clock.get_time()
         # Format the message to include the sender's ID and logical clock value
@@ -59,30 +76,38 @@ class VirtualMachine:
         # Log the sent message
         self.log_file.write(f"Machine {self.id} Sent message {message} at global time {time.time()} with current logical clock {self.clock.get_time()}\n")
         self.log_file.flush()
+    
     def run(self,ports):
+        """Run the virtual machine."""
         sockets = self.connect(ports)
         while True:
             if len(self.queue) > 0:
+                # If there are messages in the queue, process them
                 data_val, time_val = self.queue.pop(0)
                 self.clock.update(int(time_val))
-                self.log_file.write(f"Machine {self.id} received message from Machine {data_val} whose clocktime when sending the message is {time_val} at global time {time.time()}. Current logical clock for this machine: {self.clock.get_time()} with current queue length : {len(self.queue)}\n")
+                self.log_file.write(f"Machine {self.id} received message from Machine {data_val} whose logical clock time when sending the message is {time_val} at global time {time.time()}. Current logical clock for this machine: {self.clock.get_time()} with current queue length: {len(self.queue)}\n")
                 self.log_file.flush()
             else:
-                rand_val = random.randint(1, 10)
-                if rand_val == 1:
-                    self.send_message(sockets[0])
-                elif rand_val == 2:
-                    self.send_message(sockets[1])
-                elif rand_val == 3:
-                    self.send_message(sockets[0])
-                    self.clock.wait()
-                    self.send_message(sockets[1])
-                else:
-                    self.clock.increment()
-                    self.log_file.write(f"Machine {self.id} internal event at global time {time.time()} with current logical lock: {self.clock.get_time()}\n")
-                    self.log_file.flush()
+                # Otherwise, randomly send a message, wait, or increment the clock
+                rand_val = random.randint(1,10)
+            if rand_val == 1:
+                self.send_message(sockets[0])
+            elif rand_val == 2:
+                self.send_message(sockets[1])
+            elif rand_val == 3:
+                # If rand_val is 3, send a message to the first machine, wait for a random amount of time, and then send a message to the second machine
+                self.send_message(sockets[0])
+                self.clock.wait()
+                self.send_message(sockets[1])
+            else:
+                # If rand_val is greater than 3, increment the clock and log an internal event
+                self.clock.increment()
+                self.log_file.write(f"Machine {self.id} internal event at global time {time.time()} with current logical lock: {self.clock.get_time()}\n")
+                self.log_file.flush()
+        # Wait for a certain amount of time based on the tick rate
             self.clock.wait()
 def init_machine(vm):
+    """Initialize the server-side logic on the virtual machine."""
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     PORT = int(vm.ports[0])
     HOST = str(vm.host)
@@ -93,7 +118,9 @@ def init_machine(vm):
         conn, addr = s.accept()
         threading.Thread(target=vm.receive_messages, args=(conn,)).start()
         print(f"Connection accepted for vm {vm.id} at global time {time.time()} with logical clock {vm.clock.get_time()}\n")
+
 def machine(config, idx):
+    """Create and run a virtual machine with the specified configuration and ID."""
     vm = VirtualMachine(config, idx)
     init_thread = threading.Thread(target=init_machine, args=(vm,))
     init_thread.start()
@@ -102,17 +129,24 @@ def machine(config, idx):
     # Extensible to multiple producers
     prod_thread = threading.Thread(target=vm.run, args=(config[2:],))
     prod_thread.start()
+
+
 if __name__ == '__main__':
+    # Set up the configurations for the virtual machines
     localHost = "127.0.0.1"
     port1 = 2051
     port2 = 3051
     port3 = 4051
+    # Create and start a process for each virtual machine
     config1 = [localHost, port1, port2, port3]
     p1 = Process(target=machine, args=(config1, 0))
     config2 = [localHost, port2,port3, port1]
     p2 = Process(target=machine, args=(config2, 1))
     config3 = [localHost, port3,port1, port2 ]
     p3 = Process(target=machine, args=(config3, 2))
+
+    # Wait for all processes to complete
+    
     p1.start()
     p2.start()
     p3.start()
